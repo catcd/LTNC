e5a8ccde70929b5f63a9e5d334dff55cfde7db80
Duy-Cat Can (W) <catcd@vnu.edu.vn>
2021-03-06 14:31:21 +0700

    Sencond commit from Windows

--- TinhTienHang.cpp
+++ TinhTienHang.cpp
@@ -7,8 +7,10 @@ int main() {
     int dg, sl, tienHang, phiShip;
     for (int i=0; i<n; i++) {
         cin >> dg >> sl;
-        // dieu_kien ? if_true : if_false
-        dg *= sl >= 50 ? 0.85 : 1;
+        // dg *= sl >= 50 ? 0.85 : 1;
+        if (sl >= 50) {
+            dg *= 0.85;
+        }
         tienHang = dg*sl;
         phiShip = tienHang >= 10000000 ? 0 : (
             0.05*tienHang >= 200000 ? 200000 : 0.05*tienHang
